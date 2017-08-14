82579d08896ca302e3bf86a9df0aaff268ebe7db
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2017-08-14 15:32:34 +1200

    displayed 3x3  board

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,72 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+ var board = {
+   cells: [
+     { //1
+       hidden: true,
+       row: 1,
+       col: 1,
+       isMine: true
+     },
+     { //2
+       hidden: true,
+       row: 1,
+       col: 2,
+       isMine: true
+     },
+     { //3
+       hidden: true,
+       row: 1,
+       col: 3,
+       isMine: true
+     },
+     { //4
+       hidden: true,
+       row: 2,
+       col: 1,
+       isMine: true
+     },
+     { //5
+       hidden: true,
+       row: 2,
+       col: 2,
+       isMine: true
+     },
+     { //6
+       hidden: true,
+       row: 2,
+       col: 3,
+       isMine: true
+     },
+     { //7
+       hidden: true,
+       row: 3,
+       col: 1,
+       isMine: true
+     },
+     { //8
+       hidden: true,
+       row: 3,
+       col: 2,
+       isMine: true
+     },
+     { //9
+       hidden: true,
+       row: 3,
+       col: 3,
+       isMine: true
+     }
+   ]
+ }
+ /*
+ Looks Like:
+ 1 | 2 | 3
+---+---+---
+ 4 | 5 | 6
+---+---+---
+ 7 | 8 | 9
+ */
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
@@ -21,12 +86,11 @@ function checkForWin () {
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
+// cells yourself! Just use `lib.getSurroundingCells`:
 //
 //   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 //
-// It will return cell objects in an array. You should loop through 
+// It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
 }
-
